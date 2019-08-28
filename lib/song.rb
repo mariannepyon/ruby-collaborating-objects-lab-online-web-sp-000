9383dea36220c0004642696974e4d298a4c1c374
Marianne Pyon <mariannepyon@gmail.com>
2019-08-28 20:21:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,12 +17,11 @@ class Song
 
 
   def self.new_by_filename(file)
-      artist_name = file.split(" - ")[0]
-      song_name = file.split(" - ")[1]
-      song = Song.new(song_name)
-      song.artist_name = artist_name
-      song
-    end
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
+  end
 
   def save
    @@all << self
