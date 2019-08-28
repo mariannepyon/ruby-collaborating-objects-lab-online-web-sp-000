71905ec463900e137858e2ab9e3534487fa67b61
Marianne Pyon <mariannepyon@gmail.com>
2019-08-28 20:31:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
     song_info = file.chomp(".mp3").split(" - ")
     song = Song.new(song_info[1])
     song.artist_name = song_info[0]
-    song
+    song.artist_name
   end
 
   def artist_name=(name)
